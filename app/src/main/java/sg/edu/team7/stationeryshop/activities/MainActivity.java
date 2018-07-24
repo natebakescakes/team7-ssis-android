ed436fdd2ea09b940779657f3a8ce6206833b181
natebakescakes <natebakescakes@gmail.com>
2018-07-24 18:36:35 +0800

    Add all required structural fragments and populate navigation drawer, add settings menu

--- app/src/main/java/sg/edu/team7/stationeryshop/activities/MainActivity.java
+++ app/src/main/java/sg/edu/team7/stationeryshop/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package sg.edu.team7.stationeryshop.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -20,9 +23,23 @@ import java.util.List;
 import java.util.Map;
 
 import sg.edu.team7.stationeryshop.R;
+import sg.edu.team7.stationeryshop.fragments.DepartmentRepresentativeFragment;
+import sg.edu.team7.stationeryshop.fragments.DisbursementFragment;
+import sg.edu.team7.stationeryshop.fragments.ManagerDelegationFragment;
+import sg.edu.team7.stationeryshop.fragments.RequisitionRequestFragment;
+import sg.edu.team7.stationeryshop.fragments.StationeryRetrievalFragment;
+import sg.edu.team7.stationeryshop.fragments.StockAdjustmentFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        RequisitionRequestFragment.OnFragmentInteractionListener,
+        DepartmentRepresentativeFragment.OnFragmentInteractionListener,
+        ManagerDelegationFragment.OnFragmentInteractionListener,
+        StationeryRetrievalFragment.OnFragmentInteractionListener,
+        DisbursementFragment.OnFragmentInteractionListener,
+        StockAdjustmentFragment.OnFragmentInteractionListener {
+
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +48,36 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Get roles from token
+        Map token = (Map) getIntent().getSerializableExtra("accessToken");
+        List rolesFromToken = (List) token.get("roles");
+
+        List<String> roles = new ArrayList<>();
+        rolesFromToken.forEach(x -> roles.add((String) x));
+
+        // Set Starting Fragment
+        currentFragment = null;
+        Class fragmentClass;
+
+        if (roles.stream()
+                .filter(role -> role.equals("Employee") || role.equals("DepartmentHead"))
+                .count() > 0)
+            fragmentClass = RequisitionRequestFragment.class;
+        else
+            fragmentClass = StationeryRetrievalFragment.class;
+
+        try {
+            currentFragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, currentFragment).commit();
+
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -48,21 +87,20 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
 
-        // Get roles from token and fill Navigation Menu Items
-        Map token = (Map) getIntent().getSerializableExtra("accessToken");
-        List rolesFromToken = (List) token.get("roles");
-
-        List<String> roles = new ArrayList<>();
-        rolesFromToken.forEach(x -> roles.add((String) x));
-
+        // Fill Navigation Menu Items based on Roles
         if (roles.stream()
                 .filter(role -> role.equals("Employee") || role.equals("DepartmentHead"))
                 .count() > 0) {
             navigationView.getMenu().clear();
             navigationView.inflateMenu(R.menu.activity_main_department_drawer);
-        } else {
+        } else if (roles.stream()
+                .filter(role -> role.equals("StoreClerk") || role.equals("StoreManager"))
+                .count() > 0) {
             navigationView.getMenu().clear();
             navigationView.inflateMenu(R.menu.activity_main_store_drawer);
+        } else {
+            navigationView.getMenu().clear();
+            navigationView.inflateMenu(R.menu.activity_main_admin_drawer);
         }
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -91,17 +129,15 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.menu_option_1:
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -112,27 +148,44 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        Fragment fragment = null;
+        Class fragmentClass = null;
+
         if (id == R.id.nav_stationery_retrievals) {
-
+            fragmentClass = StationeryRetrievalFragment.class;
         } else if (id == R.id.nav_disbursements) {
-
+            fragmentClass = DisbursementFragment.class;
         } else if (id == R.id.nav_stock_adjustments) {
-
+            fragmentClass = StockAdjustmentFragment.class;
         } else if (id == R.id.nav_requisition_request) {
-
+            fragmentClass = RequisitionRequestFragment.class;
         } else if (id == R.id.nav_department_representative) {
-
+            fragmentClass = DepartmentRepresentativeFragment.class;
         } else if (id == R.id.nav_manager_delegation) {
-
+            fragmentClass = ManagerDelegationFragment.class;
         } else if (id == R.id.nav_settings) {
-
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_logout) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         }
 
+        try {
+            currentFragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, currentFragment).commit();
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/sg/edu/team7/stationeryshop/activities/SettingsActivity.java
@@ -0,0 +1,19 @@
+package sg.edu.team7.stationeryshop.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import sg.edu.team7.stationeryshop.R;
+
+public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
