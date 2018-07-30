f90f00697f5b4930be057c55e06e93dfb686b78f
weinchester <weinchester@gmail.com>
2018-07-30 10:35:01 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/team7/stationeryshop/models/StockAdjustment.java
@@ -0,0 +1,11 @@
+package sg.edu.team7.stationeryshop.models;
+
+import java.util.HashMap;
+import java.io.Serializable;
+
+public class StockAdjustment extends HashMap implements Serializable {
+    public StockAdjustment()
+    {
+
+    }
+}
